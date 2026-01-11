af4808a17f241fc94766827e571d759316c96614
Amna Arshad <amnamarshad33@gmail.com>
2026-01-12 04:19:36 +0500

    First part of Project completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amna Arshad",
+    author_email="amnamarshad33@gmail.com",
+    packages=find_packages()
+)
